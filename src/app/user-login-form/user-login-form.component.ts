a9734446ce1c5c1d99af4d5fab1e3c0b3d8d0a0d
Minhaj Islam <minhajislam674@gmail.com>
2023-04-01 14:19:45 +0200

    Implemented client side form validation

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -7,23 +7,22 @@ import { FetchDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  styleUrls: ['./user-login-form.component.scss'],
 })
 export class UserLoginFormComponent {
+  @Input() userData = { email: '', password: '' };
 
-  @Input() userData = {email: '', password: ''};
+  hidePassword = true;
 
   constructor(
     public fetchApiData: FetchDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar,
     private router: Router
-    ) { }
-    
+  ) {}
 
   ngOnInit(): void {}
 
@@ -31,26 +30,27 @@ export class UserLoginFormComponent {
    * Sending the form inputs to the API to log in user
    * @function loginUser
    */
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
 
   loginUser(): void {
-     this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (response) => {
         // Logic for a successful user login goes here!
         this.dialogRef.close(); //close the modal on success
-        console.log(response);
         localStorage.setItem('user', response.user.id);
         localStorage.setItem('token', response.token);
         this.router.navigate(['home']);
         this.snackBar.open('Logged in successfully!', 'OK', {
-          duration: 2000 
+          duration: 2000,
         });
-     }, (response) => {
-        console.log(response);
-       
-
-        this.snackBar.open(response, 'OK', {
-          duration: 2000 
+      },
+      (response) => {
+        this.snackBar.open('Incorrect Username or Password', 'Error', {
+          duration: 2000,
         });
-     })
+      }
+    );
   }
-
 }
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input  } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -6,29 +6,45 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+  styleUrls: ['./user-registration-form.component.scss'],
 })
 export class UserRegistrationFormComponent implements OnInit {
   @Input() userData = { username: '', email: '', password: '' };
+
+  hidePassword = true;
 
   constructor(
     public fetchApiData: FetchDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar
-  ) { }
+  ) {}
 
-ngOnInit(): void {
+  ngOnInit(): void {}
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
   }
 
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
-  this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-// Logic for a successful user registration goes here! (To be implemented)
-   this.dialogRef.close(); // This will close the modal on success!
-   this.snackBar.open('User registered successfully! Please log in to continue.', 'OK', {
-      duration: 2000
-   });
-  });
-}
-
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe(
+      (result) => {
+        // Logic for a successful user registration goes here! (To be implemented)
+        this.dialogRef.close(); // This will close the modal on success!
+        this.snackBar.open(
+          'User registered successfully! Please log in to continue.',
+          'OK',
+          {
+            duration: 2000,
+          }
+        );
+      },
+      (error) => {
+        console.error(error);
+        this.snackBar.open('This email is already taken', 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
 }
